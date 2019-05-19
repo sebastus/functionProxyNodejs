5802bfb42f96576dd740048b70f641da0d05b308
Greg Oliver <golive@microsoft.com>
2019-05-19 13:32:00 +0100

    util.format

--- proxyHttpTrigger/index.js
+++ proxyHttpTrigger/index.js
@@ -3,6 +3,13 @@ const express = require("express");
 const passport = require('passport');
 const request = require('request');
 
+var tenantId = process.env.TENANT_ID;
+var clientId = process.env.CLIENT_ID;
+var audience = process.env.AUDIENCE;
+
+var s1 = util.format("https://login.microsoftonline.com/%s/v2.0/.well-known/openid-configuration", tenantId);
+var s2 = util.format("https://sts.windows.net/%s/", tenantId);
+
 var BearerStrategy = require("passport-azure-ad").BearerStrategy;
 var bearerStrategyOptions = {
     identityMetadata: "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/v2.0/.well-known/openid-configuration",
@@ -42,6 +49,8 @@ app.post(
         // console.log("Validated claims: ", JSON.stringify(claims));
 
         console.log("Bearer strategy options: ", JSON.stringify(bearerStrategyOptions));
+        console.log("s1: ", s1);
+        console.log("s2: ", s2);
 
         options.body = JSON.stringify(req.body);
         // console.log('body text: ', JSON.stringify(req.body));
