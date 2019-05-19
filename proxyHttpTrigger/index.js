5ce56267d769edc428177e88f1122257d87352bf
Greg Oliver <golive@microsoft.com>
2019-05-19 12:41:09 +0100

    rearrange

--- proxyHttpTrigger/index.js
+++ proxyHttpTrigger/index.js
@@ -8,7 +8,7 @@ var clientId = process.env.CLIENT_ID;
 var audience = process.env.AUDIENCE;
 
 var BearerStrategy = require("passport-azure-ad").BearerStrategy;
-var options = {
+var bearerStrategyOptions = {
     identityMetadata: util.format("https://login.microsoftonline.com/%s/v2.0/.well-known/openid-configuration", tenantId),
     clientID: clientId,
     issuer: util.format("https://sts.windows.net/%s/", tenantId),
@@ -17,20 +17,19 @@ var options = {
     passReqToCallback: false
 };
 
-console.log("Bearer strategy options: ", JSON.stringify(options));
-
-var bearerStrategy = new BearerStrategy(options, function (token, done) {
-    done(null, {}, token);
-});
-
 const app = express();
-
 app.use(require('morgan')('combined'));
 app.use(require('body-parser').urlencoded({"extended":true}));
+
 app.use(passport.initialize());
+var bearerStrategy = new BearerStrategy(bearerStrategyOptions, function (token, done) {
+    done(null, {}, token);
+});
 passport.use(bearerStrategy);
 
-var options = {
+console.log("Bearer strategy options: ", JSON.stringify(bearerStrategyOptions));
+
+var requestOptions = {
     url: process.env.SPLUNK_ADDRESS,
     headers: {
         'Authorization': 'Splunk ' + process.env.SPLUNK_TOKEN
@@ -45,10 +44,10 @@ app.post(
         // console.log("User info: ", req.user);
         // console.log("Validated claims: ", JSON.stringify(claims));
 
-        options.body = JSON.stringify(req.body);
+        requestOptions.body = JSON.stringify(req.body);
         // console.log('body text: ', JSON.stringify(req.body));
 
-        request.post(options, function (error, response, body) {
+        request.post(requestOptions, function (error, response, body) {
             if (error) {
                 console.error('error:', error); // Print the error if one occurred
                 res.status(500);
