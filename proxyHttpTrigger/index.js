e853620816831fbc491459613c5f5dce8943dc3e
Greg Oliver <golive@microsoft.com>
2019-05-20 16:36:54 +0100

    fix authInfo

--- proxyHttpTrigger/index.js
+++ proxyHttpTrigger/index.js
@@ -53,13 +53,13 @@ app.post(
     passport.authenticate("oauth-bearer", { session: false }),
     function (req, res) {
 
-        console.log("Bearer strategy options: ", JSON.stringify(bearerStrategyOptions));
-        console.log("AuthInfo: ", JSON.stringify(req.authinfo));
+        // console.log("Bearer strategy options: ", JSON.stringify(bearerStrategyOptions));
+        console.log("AuthInfo: ", JSON.stringify(req.authInfo));
         // console.log("s1: ", s1);
         // console.log("s2: ", s2);
 
         options.body = JSON.stringify(req.body);
-        console.log('body text: ', JSON.stringify(req.body));
+        // console.log('body text: ', JSON.stringify(req.body));
 
         request.post(options, function (error, response, body) {
             if (error) {
